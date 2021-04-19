3ef985e0ded59f14927d799ce8ad6b80cc2f2501
Tom <fougeretom@gmail.com>
2021-04-19 17:45:54 +0200

    Fix issue with full update and partial update (split datetime to hour and minute)

--- epaper_display.py
+++ epaper_display.py
@@ -6,34 +6,36 @@ class EPaper:
 
     def __init__(self):
         self.epd = epd2in13.EPD()
+        self.epd.init(self.epd.lut_full_update)
         self.image = None
-        self.is_full_updated = False
-        self.datetime = datetime.min
+        self.is_full_updated = True
+        self.hour = 0
+        self.minute = 0
 
     def update(self, one_datetime, events, force_update=False):
 
         # Select screen mode (full or partial)
         if self.need_full_update(one_datetime, force_update):
+            self.hour = one_datetime.hour
             self.set_full_update()
         elif self.is_full_updated:
             self.set_partial_update()
 
-        # Change hour
-        self.datetime = one_datetime
-
-        # Display new screen
         if self.need_new_screen(one_datetime, force_update):
+            self.minute = one_datetime.minute
+
+            # Display new screen
             self.set_new_screen(one_datetime, events)
             self.epd.sleep()
 
     def need_full_update(self, one_datetime, force_update=False):
-        if one_datetime >= self.datetime + timedelta(hours=1) or force_update == True:
+        if one_datetime.hour > self.hour or (one_datetime.hour == 0 and self.hour == 23) or force_update == True:
             return True
         else:
             return False
 
     def need_new_screen(self, one_datetime, force_update=False):
-        if one_datetime >= self.datetime + timedelta(minutes=1) or force_update == True:
+        if one_datetime.minute > self.minute or (one_datetime.minute == 0 and self.minute == 59) or force_update == True:
             return True
         else:
             return False
--- test_epaper.py
+++ test_epaper.py
@@ -5,8 +5,9 @@ from datetime import datetime, timedelta
 display_test = EPaper()
 
 def test_need_full_update():
-    now = datetime.now()
-    display_test.datetime = now
+    now = datetime(2021, 4, 19, 20, 00, 00)
+    display_test.hour = 20
+    display_test.minute = 0
 
     one_datetime = now + timedelta(minutes=59)
     assert display_test.need_full_update(one_datetime) == False
@@ -16,8 +17,9 @@ def test_need_full_update():
 
 
 def test_need_new_screen():
-    now = datetime.now()
-    display_test.datetime = now
+    now = datetime(2021, 4, 19, 20, 00, 00)
+    display_test.hour = 20
+    display_test.minute = 0
 
     one_datetime = now + timedelta(seconds=59)
     assert display_test.need_new_screen(one_datetime) == False
@@ -28,8 +30,52 @@ def test_need_new_screen():
     one_datetime = now + timedelta(minutes=1)
     assert display_test.need_new_screen(one_datetime) == True
 
+def test_update():
+    now = datetime(2021, 4, 19, 20, 00, 00)
+    display_test.hour = 20
+    display_test.minute = 0
+
+    # Few seconds later
+    one_datetime = now + timedelta(seconds=5)
+    display_test.update(one_datetime, [])
+
+    assert display_test.is_full_updated is False
+    assert display_test.hour == 20
+    assert display_test.minute == 0
+
+    # One minute later
+    one_datetime = now + timedelta(seconds=65)
+    display_test.update(one_datetime, [])
+
+    assert display_test.is_full_updated is False
+    assert display_test.hour == 20
+    assert display_test.minute == 1
+
+    # One hour later
+    one_datetime = now + timedelta(minutes=65)
+    display_test.update(one_datetime, [])
+
+    assert display_test.is_full_updated is True
+    assert display_test.hour == 21
+    assert display_test.minute == 5
+
+    # One hour and one minute later
+    one_datetime = now + timedelta(minutes=66)
+    display_test.update(one_datetime, [])
+
+    assert display_test.is_full_updated is False
+    assert display_test.hour == 21
+    assert display_test.minute == 6
+
+    # One hour and 2 minutes later + force update
+    # one_datetime = now + timedelta(minutes=67)
+    # display_test.update(one_datetime, [], force_update=True)
+    #
+    # assert display_test.is_full_updated is True
+    # assert display_test.datetime == one_datetime
 
 if __name__ == '__main__':
     test_need_full_update()
     test_need_new_screen()
+    test_update()
 
