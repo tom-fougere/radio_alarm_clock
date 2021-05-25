90c04ea2fb859663018181260737c208c0f37c76
Tom <fougeretom@gmail.com>
2021-05-25 11:37:43 +0200

    Fix OnlineCalendar with new events

--- AlarmCalendar.py
+++ AlarmCalendar.py
@@ -7,7 +7,7 @@ FORCE_STRING = '#force'
 
 logger = logging.getLogger(__name__)
 
-class AlarmCalendar:
+class OnlineCalendar:
 
     def __init__(self):
         self.google_service = GoogleCalendarAPI()
@@ -39,7 +39,7 @@ class AlarmCalendar:
         :param reset_hour: If True, the hour is not taken into account
         :return:
             - is_alarm_today: boolean for alarm to the selected datetime
-            - alarm_event: alarm associated
+            - alarm_event: first (sorted) event of the date
         """
 
         # Get the events from the three calendars
@@ -60,14 +60,11 @@ class AlarmCalendar:
         sorted_events = sort_events(events_alarm_calendar, events_public_holiday_calendar, events_personal_calendar)
         logger.debug('Sorted Events from Google calendars (%s): %s', today_datetime, sorted_events)
 
-        is_alarm_today = []
-        for event in sorted_events:
-            if event.name == 'Alarm':
-                is_alarm_today.append(True)
-            else:
-                is_alarm_today.append(False)
+        is_alarm_today = False
+        if len(sorted_events) > 0 and sorted_events[0].name == 'Alarm':
+            is_alarm_today = True
 
-        return is_alarm_today, sorted_events
+        return is_alarm_today, sorted_events[0]
 
     def is_alarm_tomorrow(self, today_datetime):
         """
@@ -87,71 +84,36 @@ class AlarmCalendar:
 
         return is_alarm_tomorrow, alarm_event
 
+    def get_events(self, today_datetime, reset_hour=False):
+        """
+        Get events from the 3 calendars at the specified datetime
 
-def get_first_event(events, priority_string=''):
-    """
-    Get the first event of a list of events
-    If the event have the substring 'priority_string' in the name, it'll be selected
+        :param today_datetime: Current datetime
+        :param reset_hour: If True, the hour is not taken into account
+        :return:
+            - sorted_events: list of events, Events
+        """
 
-    :param events: List of Events, Google service "Event"
-    :param priority_string: Substring to search in event name, string
-    :return:
-        - is_priority_event: Is the first element part of a priority, Boolean
-        - event: First event, Google service "Event"
-    """
-    is_priority_event = False
+        # Get the events from the three calendars
+        events_alarm_calendar = self.google_service.get_events_from_day(self.alarm_calendar,
+                                                                        today_datetime,
+                                                                        reset_hour=reset_hour)
 
-    events_list = []
+        events_public_holiday_calendar = self.google_service.get_events_from_day(self.public_holiday_calendar,
+                                                                                 today_datetime)
 
-    for event in events['items']:
-        if priority_string in event['summary'].lower():
-            is_priority_event = True
-            events_list.insert(0, event)
-        else:
-            events_list.append(event)
+        events_personal_calendar = self.google_service.get_events_from_day(self.personal_calendar,
+                                                                           today_datetime)
 
-    event = events_list[0] if len(events_list) > 0 else []
-    return is_priority_event, event
+        # logger.debug('Events from Alarm calendar (%s): %s', today_datetime, events_alarm_calendar)
+        # logger.debug('Events from Public Holiday calendar (%s): %s', today_datetime, events_public_holiday_calendar)
+        # logger.debug('Events from Personal calendar (%s): %s', today_datetime, events_personal_calendar)
 
+        sorted_events = sort_events(events_alarm_calendar, events_public_holiday_calendar, events_personal_calendar)
+        logger.debug('Sorted Events from Google calendars (%s): %s', today_datetime, sorted_events)
 
-def get_highest_priority_event(events_alarm_calendar, events_public_holiday_calendar, events_personal_calendar):
-    """
-    Get the highest priority event in all calendars (alarm calendar, public holiday calendar et personal calendar)
-    The highest priority event corresponds to the event where the alarm should ring
+        return sorted_events
 
-    :param events_alarm_calendar: List of Events from the alarm calendar, Google service "Event"
-    :param events_public_holiday_calendar: List of Events from the public holiday calendar, Google service "Event"
-    :param events_personal_calendar: List of Events from the personal calendar, Google service "Event"
-    :return:
-        - is_alarm_today: Is the event to trigger a ringing, Boolean
-        - alarm_event: (Associated) Highest priority event, Google service "Event"
-    """
-    is_alarm = True if len(events_alarm_calendar['items']) > 0 else False
-    is_force_alarm, force_event = get_first_event(events_alarm_calendar, FORCE_STRING)
-    is_off_event, off_event = get_first_event(events_personal_calendar, OFF_STRING)
-    is_public_holiday, public_holiday_event = get_first_event(events_public_holiday_calendar)
-
-    is_alarm_today = is_force_alarm or (is_alarm and (not is_off_event and not is_public_holiday))
-
-    alarm_event = None
-    if is_force_alarm:
-        alarm_event = force_event
-    elif is_alarm and (not is_off_event and not is_public_holiday):
-        alarm_event = events_alarm_calendar['items'][0]
-    elif is_off_event:
-        alarm_event = off_event
-    elif is_public_holiday:
-        alarm_event = public_holiday_event
-
-    # Save the event from the personal calendar even if there is no alarm
-    elif len(events_personal_calendar['items']) > 0:
-        alarm_event = off_event
-
-    # Save the event from the public holiday calendar even if there is no alarm
-    elif len(events_public_holiday_calendar['items']) > 0:
-        alarm_event = public_holiday_event
-
-    return is_alarm_today, alarm_event
 
 def sort_events(events_alarm_calendar, events_public_holiday_calendar, events_personal_calendar):
     """
@@ -172,7 +134,7 @@ def sort_events(events_alarm_calendar, events_public_holiday_calendar, events_pe
     events_public_holiday = convert_google_events_to_calendar_events(events_public_holiday_calendar, name=public_holiday_calendar_name)
     events_personal = convert_google_events_to_calendar_events(events_personal_calendar, name=personal_calendar_name)
 
-    full_list = events_alarm + events_public_holiday + events_personal
+    full_list = events_public_holiday + events_alarm + events_personal
     sorted_events = []
 
     priority = 0
@@ -180,36 +142,41 @@ def sort_events(events_alarm_calendar, events_public_holiday_calendar, events_pe
     while len(full_list) > 0:
         event = full_list[index]
 
-        if priority == 0:
+        if event.kind == 'None' and priority < 5:
+            index +=1
+        elif priority == 0:
             if event.title.lower().find(FORCE_STRING)>=0:  # FORCE
                 sorted_events.append(event)
                 full_list.pop(index)
             else:
                 index += 1
-        if priority == 1:
+        elif priority == 1:
             if event.title.lower().find(OFF_STRING) >= 0:  # OFF
                 sorted_events.append(event)
                 full_list.pop(index)
             else:
                 index += 1
-        if priority == 2:
+        elif priority == 2:
             if event.name == public_holiday_calendar_name:
                 sorted_events.append(event)
                 full_list.pop(index)
             else:
                 index += 1
-        if priority == 3:
+        elif priority == 3:
             if event.name == alarm_calendar_name:
                 sorted_events.append(event)
                 full_list.pop(index)
             else:
                 index += 1
-        if priority == 4:
+        elif priority == 4:
             if event.name == personal_calendar_name:
                 sorted_events.append(event)
                 full_list.pop(index)
             else:
                 index += 1
+        if priority == 5:
+            sorted_events.append(event)
+            full_list.pop(index)
 
         if index >= len(full_list):
             priority +=1
--- test_alarmCalendar.py
+++ test_alarmCalendar.py
@@ -3,8 +3,61 @@ import datetime
 from AlarmCalendar import *
 from documents.rw_dict import *
 
-myCalendar = AlarmCalendar()
+myCalendar = OnlineCalendar()
 my_calendars = read_dict_file('documents/my_calendars.txt')
+
+alarm_calendar_name = 'Alarm'
+public_holiday_calendar_name = 'Public Holiday'
+personal_calendar_name = 'Personal'
+
+def test_get_events():
+    myCalendar.set_calendars(alarm_calendar=my_calendars['Reveil'],
+                             public_holiday_calendar=my_calendars['Jours Feries'],
+                             personal_calendar=my_calendars['Elise et Tom'])
+
+    date1 = datetime.datetime(2020, 3, 24)
+    events1 = myCalendar.get_events(date1)
+    assert len(events1) == 3
+    assert (events1[0].kind == 'None' and events1[0].name == public_holiday_calendar_name)
+    assert (events1[1].kind == 'None' and events1[1].name == alarm_calendar_name)
+    assert (events1[2].kind == 'None' and events1[2].name == personal_calendar_name)
+
+    date2 = datetime.datetime(2020, 3, 25)
+    events2 = myCalendar.get_events(date2)
+    assert len(events2) == 3
+    assert events2[0].title == 'Reveil 1'
+    assert (events2[1].kind == 'None' and events2[1].name == public_holiday_calendar_name)
+    assert (events2[2].kind == 'None' and events2[2].name == personal_calendar_name)
+
+    date3 = datetime.datetime(2020, 3, 27)
+    events3 = myCalendar.get_events(date3)
+    assert len(events3) == 3
+    assert events3[0].title == '#off congé 1'
+    assert (events3[1].kind == 'None' and events3[1].name == public_holiday_calendar_name)
+    assert (events3[2].kind == 'None' and events3[2].name == alarm_calendar_name)
+
+    date4 = datetime.datetime(2020, 3, 29)
+    events4 = myCalendar.get_events(date4)
+    assert len(events4) == 3
+    assert events4[0].title == 'Heure d\'été'
+    assert (events4[1].kind == 'None' and events4[1].name == alarm_calendar_name)
+    assert (events4[2].kind == 'None' and events4[2].name == personal_calendar_name)
+
+    date5 = datetime.datetime(2020, 4, 2)
+    events5 = myCalendar.get_events(date5)
+    assert len(events5) == 3
+    assert events5[0].title == '#FORCE Reveil 3'
+    assert events5[1].title == '#OFF Congé 3'
+    assert (events5[2].kind == 'None' and events5[2].name == public_holiday_calendar_name)
+
+    date6 = datetime.datetime(2020, 4, 4)
+    events6 = myCalendar.get_events(date6)
+    assert len(events6) == 5
+    assert events6[0].title == '#force reveil'
+    assert events6[1].title == '#off vac'
+    assert events6[2].title == 'reveil pas force'
+    assert events6[3].title == 'rdv'
+    assert (events6[4].kind == 'None' and events6[4].name == public_holiday_calendar_name)
 
 
 def test_alarm():
@@ -14,40 +67,45 @@ def test_alarm():
 
     date1 = datetime.datetime(2020, 3, 24)
     is_alarm1, event1 = myCalendar.is_alarm_today(date1)
-    assert len(is_alarm1) == 0
-    assert len(event1) == 0
+    assert is_alarm1 is False
+    assert event1.kind == 'None'
 
     date2 = datetime.datetime(2020, 3, 25)
     is_alarm2, event2 = myCalendar.is_alarm_today(date2)
-    assert is_alarm2[0] is True
-    assert event2[0].title == 'Reveil 1'
+    assert is_alarm2 is True
+    assert event2.title == 'Reveil 1'
 
     date3 = datetime.datetime(2020, 3, 26)
     is_alarm3, event3 = myCalendar.is_alarm_today(date3)
-    assert is_alarm3[0] is True
-    assert event3[0].title == '#force Reveil 2'
+    assert is_alarm3 is True
+    assert event3.title == '#force Reveil 2'
 
     date4 = datetime.datetime(2020, 3, 27)
     is_alarm4, event4 = myCalendar.is_alarm_today(date4)
-    assert is_alarm4[0] is False
-    assert event4[0].title == '#off congé 1'
+    assert is_alarm4 is False
+    assert event4.title == '#off congé 1'
 
     date5 = datetime.datetime(2020, 3, 28)
     is_alarm5, event5 = myCalendar.is_alarm_today(date5)
-    assert is_alarm5[0] is False
-    assert event5[0].title == 'congé 2'
+    assert is_alarm5 is False
+    assert event5.title == 'congé 2'
 
     date6 = datetime.datetime(2020, 3, 29)
     is_alarm6, event6 = myCalendar.is_alarm_today(date6)
-    assert is_alarm6[0] is False
-    assert event6[0].title == 'Heure d\'été'
+    assert is_alarm6 is False
+    assert event6.title == 'Heure d\'été'
 
     date7 = datetime.datetime(2020, 4, 2)
     is_alarm7, event7 = myCalendar.is_alarm_today(date7)
-    assert is_alarm7[0] is True
-    assert event7[0].title == '#FORCE Reveil 3'
+    assert is_alarm7 is True
+    assert event7.title == '#FORCE Reveil 3'
 
     date8 = datetime.datetime(2020, 4, 3)
     is_alarm8, event8 = myCalendar.is_alarm_today(date8)
-    assert is_alarm8[0] is False
-    assert event8[0].title == '#OFF Congé 3'
+    assert is_alarm8 is False
+    assert event8.title == '#OFF Congé 3'
+
+    date9 = datetime.datetime(2020, 4, 4)
+    is_alarm9, event9 = myCalendar.is_alarm_today(date9)
+    assert is_alarm9 is True
+    assert event9.title == '#force reveil'
